94f3cc733a54d7ee3f7f392dee87a5d068957608
Borislav Gizdov <borislav.gizdov@gmail.com>
2024-10-18 08:41:27 +0300

    Added netlify proxy server

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,23 @@
 import { Language, Translation } from './types';
 
 // const API_BASE_URL = 'https://dictzilla-api.apirobots.pro/v2';
-const API_BASE_URL = 'http://localhost:3001/api';
+// const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = '/.netlify/functions';
 
 
 export async function fetchLanguages(): Promise<Language[]> {
   const response = await fetch(`${API_BASE_URL}/languages`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch languages');
+  }
   const data = await response.json();
+  console.log("response: " + data);
   return data.items;
 }
 
 export async function fetchTranslation(text: string, source: string, target: string): Promise<Translation> {
-  const response = await fetch(`${API_BASE_URL}/translations?text=${encodeURIComponent(text)}&source=${source}&target=${target}`);
+  const response = await fetch(`${API_BASE_URL}/translations`);
+    // ?text=${encodeURIComponent(text)}&source=${source}&target=${target}`);
   if (!response.ok) {
     throw new Error('Failed to fetch translation');
   }
